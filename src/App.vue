86481d45fd43af084c55757914ab5b84b76e2b56
CJWbiu <1719064252@qq.com>
2021-09-21 16:37:30 +0800

    [update]修改导航栏样式以及替换图标

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
     <n-layout>
-		<n-layout-header>
-			<n-menu :options="menuOptions"
-					mode="horizontal"
-					:value="activeKey"
-					@update:value="onActiveChange" />
+		<n-layout-header bordered class="app-main__header">
+			<n-space>
+				<n-text tag="div">
+					<img width="42" src="./assets/imgs/inspiration.png" />
+				</n-text>
+				<n-menu :options="menuOptions"
+						mode="horizontal"
+						:value="activeKey"
+						@update:value="onActiveChange" />
+			</n-space>
 		</n-layout-header>
 		<n-layout class="app-main__content">
 			<router-view></router-view>
@@ -18,7 +23,7 @@
  * @file 入口内容
  */
 
-import { defineComponent, onMounted } from 'vue';
+import { defineComponent, onMounted, VNodeChild } from 'vue';
 import { useLoadingBar, MenuOption } from 'naive-ui'
 import { setLoadingBar } from './router';
 import { ROUTES } from './router/routers';
@@ -38,7 +43,8 @@ function getMenuOptions (): MenuOption[] {
 		.map(route => {
 			return {
 				label: (route.meta?.title || '') as string,
-				key: route.name as string
+				key: route.name as string,
+				icon: (route.meta?.icon || null) as () => VNodeChild
 			};
 		});
 	
@@ -62,5 +68,9 @@ export default defineComponent({
 <style scoped>
 	.app-main__content {
 		height: calc(100vh - 42px);
+	}
+
+	.app-main__header {
+		padding: 6px 12px;
 	}
 </style>
--- src/router/modules/tools.ts
+++ src/router/modules/tools.ts
@@ -1,12 +1,15 @@
 import { RouteRecordRaw } from 'vue-router';
 import BinaryOperation from 'pages/tools/binary-operation/index.vue';
+import { renderIcon } from '@/utils/icon';
+import { Repeat } from '@vicons/ionicons5';
 
 const TOOLS_CHILDREN: RouteRecordRaw[] = [
     {
         path: 'binary',
         name: 'binaryOperation',
         meta: {
-            title: "进制转换"
+            title: "进制转换",
+            icon: renderIcon(Repeat)
         },
         component: BinaryOperation
     }
